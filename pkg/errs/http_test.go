d656e7dc0631dc79f0c3b24022556abdaa160944
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-07-13 08:14:56 -0300

    test: pkg error http
    test: pkg error http

---
+++ pkg/errs/http_test.go
@@ -0,0 +1,42 @@
+package errs
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHttpUnprocessableEntity(t *testing.T) {
+	w := httptest.NewRecorder()
+	HttpUnprocessableEntity(w, "any message")
+
+	assert.Equal(t, http.StatusUnprocessableEntity, w.Code)
+
+	write = fakeWrite
+	defer restoreWrite(write)
+
+	HttpUnprocessableEntity(w, "any message error")
+}
+
+func TestHttpInternalServerError(t *testing.T) {
+	w := httptest.NewRecorder()
+	HttpInternalServerError(w)
+
+	assert.Equal(t, http.StatusInternalServerError, w.Code)
+}
+
+func TestHttpNotFound(t *testing.T) {
+	w := httptest.NewRecorder()
+	HttpNotFound(w)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
+}
+
+func TestHttpToManyRequests(t *testing.T) {
+	w := httptest.NewRecorder()
+	HttpToManyRequests(w)
+
+	assert.Equal(t, http.StatusTooManyRequests, w.Code)
+}
