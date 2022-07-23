89cd0c17c0766f2b025ac395a7844ce5ef6696dd
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-07-23 06:56:08 -0300

    feat: handle healthy check
    feat: handle healthy check

---
+++ pkg/api/handlers/handleok.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/rafaelsanzio/go-flashscore/pkg/applog"
+	"github.com/rafaelsanzio/go-flashscore/pkg/errs"
+)
+
+type APIOkPayload struct {
+	Health int    `json:"health,omitempty"`
+	Test   string `json:"test,omitempty"`
+}
+
+func HandleAPIOK(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	dataReturn := APIOkPayload{
+		Health: 1,
+		Test:   "Everthing is OK",
+	}
+
+	data, err_ := jsonMarshal(dataReturn)
+	if err_ != nil {
+		_ = errs.ErrMarshalingJson.Throwf(applog.Log, errs.ErrFmt, err_)
+		errs.HttpInternalServerError(w)
+		return
+	}
+
+	_, err_ = write(w, data)
+
+	if err_ != nil {
+		_ = errs.ErrResponseWriter.Throwf(applog.Log, errs.ErrFmt, err_)
+		errs.HttpInternalServerError(w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
