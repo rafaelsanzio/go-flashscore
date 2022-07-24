06d782144b7b463c904102ebb5841293101b68cb
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-07-24 08:25:12 -0300

    feat: handle get tournament
    feat: handle get tournament

---
+++ pkg/api/handlers/handlegettournament.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/rafaelsanzio/go-flashscore/pkg/applog"
+	"github.com/rafaelsanzio/go-flashscore/pkg/errs"
+	"github.com/rafaelsanzio/go-flashscore/pkg/model/repo"
+)
+
+func HandleGetTournament(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if id == "" {
+		_ = errs.ErrGettingParam.Throwf(applog.Log, errs.ErrFmt, id)
+		errs.HttpNotFound(w)
+		return
+	}
+
+	tournament, err := repo.GetTournamentRepo().Get(ctx, id)
+	if err != nil {
+		errs.HttpInternalServerError(w)
+		return
+	}
+
+	if tournament == nil {
+		_ = errs.ErrTournamentIsNotFound.Throwf(applog.Log, errs.ErrFmt, id)
+		errs.HttpNotFound(w)
+		return
+	}
+
+	data, err_ := jsonMarshal(tournament)
+	if err_ != nil {
+		_ = errs.ErrMarshalingJson.Throwf(applog.Log, errs.ErrFmt, err_)
+		errs.HttpInternalServerError(w)
+		return
+	}
+
+	_, err_ = write(w, data)
+	if err_ != nil {
+		_ = errs.ErrResponseWriter.Throwf(applog.Log, errs.ErrFmt, err_)
+		errs.HttpInternalServerError(w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
