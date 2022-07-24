d2ba479b7b0e42da2c744a76554b49d34c33521f
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-07-24 08:01:55 -0300

    feat: handle get match
    feat: handle get match

---
+++ pkg/api/handlers/handlegetmatch.go
@@ -0,0 +1,70 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/rafaelsanzio/go-flashscore/pkg/applog"
+	"github.com/rafaelsanzio/go-flashscore/pkg/errs"
+	"github.com/rafaelsanzio/go-flashscore/pkg/model/repo"
+)
+
+func HandleGetMatch(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	vars := mux.Vars(r)
+	id := vars["id"]
+	matchID := vars["match_id"]
+
+	if id == "" {
+		_ = errs.ErrGettingParam.Throwf(applog.Log, errs.ErrFmt, id)
+		errs.HttpNotFound(w)
+		return
+	}
+
+	tournament, err := repo.GetTournamentRepo().Get(ctx, id)
+	if err != nil {
+		errs.HttpInternalServerError(w)
+		return
+	}
+
+	if tournament == nil {
+		_ = errs.ErrTournamentIsNotFound.Throwf(applog.Log, errs.ErrFmt, id)
+		errs.HttpNotFound(w)
+		return
+	}
+
+	if matchID == "" {
+		_ = errs.ErrGettingParam.Throwf(applog.Log, errs.ErrFmt, id)
+		errs.HttpNotFound(w)
+		return
+	}
+
+	match, err := repo.GetMatchRepo().FindMatchForTournament(ctx, matchID, tournament.ID)
+	if err != nil {
+		errs.HttpInternalServerError(w)
+		return
+	}
+
+	if match == nil {
+		_ = errs.ErrMatchIsNotFound.Throwf(applog.Log, errs.ErrFmt, id)
+		errs.HttpNotFound(w)
+		return
+	}
+
+	data, err_ := jsonMarshal(match)
+	if err_ != nil {
+		_ = errs.ErrMarshalingJson.Throwf(applog.Log, errs.ErrFmt, err_)
+		errs.HttpInternalServerError(w)
+		return
+	}
+
+	_, err_ = write(w, data)
+	if err_ != nil {
+		_ = errs.ErrResponseWriter.Throwf(applog.Log, errs.ErrFmt, err_)
+		errs.HttpInternalServerError(w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
