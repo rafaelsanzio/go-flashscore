34c2dfde9fd5ebb998014d7d24365983521ce333
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-07-26 08:45:49 -0300

    fix: comment useless code for now
    fix: comment useless code for now

--- pkg/middleware/util.go
+++ pkg/middleware/util.go
@@ -1,13 +1,12 @@
 package middleware
 
 import (
-	"github.com/dgrijalva/jwt-go"
 	"github.com/rafaelsanzio/go-flashscore/pkg/config"
 	"github.com/rafaelsanzio/go-flashscore/pkg/config/key"
 	"github.com/rafaelsanzio/go-flashscore/pkg/errs"
 )
 
-var jwtParseWithClaims = jwt.ParseWithClaims
+/* var jwtParseWithClaims = jwt.ParseWithClaims
 
 func fakeJwtParseWithClaims(tokenString string, claims jwt.Claims, keyFunc jwt.Keyfunc) (*jwt.Token, error) {
 	return nil, errs.ErrMarshalingJson
@@ -15,7 +14,7 @@ func fakeJwtParseWithClaims(tokenString string, claims jwt.Claims, keyFunc jwt.K
 
 func restoreJwtParseWithClaims(replace func(tokenString string, claims jwt.Claims, keyFunc jwt.Keyfunc) (*jwt.Token, error)) {
 	jwtParseWithClaims = replace
-}
+} */
 
 var configValue = config.Value
 
