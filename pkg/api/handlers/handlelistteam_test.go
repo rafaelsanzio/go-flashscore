7af872efa176b41248a4ba907b822271054b77d6
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-08-10 11:02:25 -0300

    test: set cache on list teams
    test: set cache on list teams

--- pkg/api/handlers/handlelistteam_test.go
+++ pkg/api/handlers/handlelistteam_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/rafaelsanzio/go-flashscore/pkg/cache"
 	"github.com/rafaelsanzio/go-flashscore/pkg/errs"
 	"github.com/rafaelsanzio/go-flashscore/pkg/model/repo"
 	"github.com/rafaelsanzio/go-flashscore/pkg/prototype"
@@ -30,12 +31,21 @@ func mockListTeamThrowFunc(ctx context.Context) ([]team.Team, errs.AppError) {
 	return nil, errs.ErrRepoMockAction
 }
 
+func mockCacheSetFunc(ctx context.Context, key string, value []byte) error {
+	return nil
+}
+
+func mockCacheSetThrowFunc(ctx context.Context, key string, value []byte) error {
+	return errs.ErrRepoMockAction
+}
+
 func TestHandleListTeam(t *testing.T) {
 	testCases := []struct {
 		Name               string
 		HandleListTeamFunc func(ctx context.Context) ([]team.Team, errs.AppError)
 		MarshalFunc        func(v interface{}) ([]byte, error)
 		WriteFunc          func(http.ResponseWriter, []byte) (int, error)
+		CacheSetFunc       func(ctx context.Context, key string, value []byte) error
 		ExpectedStatusCode int
 	}{
 		{
@@ -43,25 +53,36 @@ func TestHandleListTeam(t *testing.T) {
 			HandleListTeamFunc: mockListTeamFunc,
 			MarshalFunc:        jsonMarshal,
 			WriteFunc:          write,
+			CacheSetFunc:       mockCacheSetFunc,
 			ExpectedStatusCode: 200,
 		}, {
 			Name:               "Throwing handle list teams",
 			HandleListTeamFunc: mockListTeamThrowFunc,
 			MarshalFunc:        jsonMarshal,
 			WriteFunc:          write,
+			CacheSetFunc:       mockCacheSetFunc,
 			ExpectedStatusCode: 500,
 		}, {
 			Name:               "Throwing error on marshal function",
 			HandleListTeamFunc: mockListTeamFunc,
 			MarshalFunc:        fakeMarshal,
 			WriteFunc:          write,
+			CacheSetFunc:       mockCacheSetFunc,
 			ExpectedStatusCode: 500,
 		}, {
 			Name:               "Throwing error on write function",
 			HandleListTeamFunc: mockListTeamFunc,
 			MarshalFunc:        jsonMarshal,
 			WriteFunc:          fakeWrite,
+			CacheSetFunc:       mockCacheSetFunc,
 			ExpectedStatusCode: 500,
+		}, {
+			Name:               "Logging error on cache set function",
+			HandleListTeamFunc: mockListTeamFunc,
+			MarshalFunc:        jsonMarshal,
+			WriteFunc:          write,
+			CacheSetFunc:       mockCacheSetThrowFunc,
+			ExpectedStatusCode: 200,
 		},
 	}
 
@@ -72,6 +93,11 @@ func TestHandleListTeam(t *testing.T) {
 			ListFunc: tc.HandleListTeamFunc,
 		})
 		defer repo.SetTeamRepo(nil)
+
+		cache.SetStore(cache.MockCacheStore{
+			SetFunc: tc.CacheSetFunc,
+		})
+		defer cache.SetStore(nil)
 
 		jsonMarshal = tc.MarshalFunc
 		defer restoreMarshal(jsonMarshal)
