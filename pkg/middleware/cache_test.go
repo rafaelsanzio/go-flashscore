7ffb93870a0b9b54f0816bdab8c6af4e0fc6c927
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-08-22 17:25:23 -0300

    test: add new unit
    test: add new unit

--- pkg/middleware/cache_test.go
+++ pkg/middleware/cache_test.go
@@ -1,7 +1,14 @@
 package middleware
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestVerifyCache(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/ok", nil)
 
+	VerifyCache(w, r)
 }
