7010f544163afe4d048f3a2a99c25d3322e22837
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-09-21 14:59:11 -0300

    feat: remove comments
    feat: remove comments

--- pkg/api/handlers/handle.go
+++ pkg/api/handlers/handle.go
@@ -33,32 +33,6 @@ func HandleAdapter(hf http.HandlerFunc) http.HandlerFunc {
 
 		middleware.VerifyCache(w, r)
 
-		/* 	// verify cache if exists
-		cacheKey := fmt.Sprintf("%s%s", r.Method, r.URL)
-		value, err_ := cache.GetStore().Get(r.Context(), cacheKey)
-		if value != nil && err_ == nil {
-			data, err_ := jsonMarshal(value)
-			if err_ != nil {
-				_ = errs.ErrMarshalingJson.Throwf(applog.Log, errs.ErrFmt, err_)
-				errs.HttpInternalServerError(w)
-				return
-			}
-
-			_, err_ = write(w, data)
-			if err_ != nil {
-				_ = errs.ErrResponseWriter.Throwf(applog.Log, errs.ErrFmt, err_)
-				errs.HttpInternalServerError(w)
-				return
-			}
-
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		if value == nil {
-			applog.Log.Warnf("Cache does not exist for this key: %s", cacheKey)
-		} */
-
 		hf(w, r)
 	}
 }
